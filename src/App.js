881e4e4251e2a7a50ce33b59935545aee361c439
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2023-11-11 13:34:13 -0300

    feat: Implementar página do buscador

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi';
+import { useState } from 'react';
+
+import api from './services/api';
+import './style.css';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+    if (input === '') {
+      alert("Preencha algum CEP!")
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json/`);
+
+      if (response.data.erro === true) {
+        alert("Preencha um CEP válido!");
+        setInput("");
+        return;
+      }
+      setCep(response.data);
+      setInput("");
+    } catch {
+      alert("Ops erro ao buscar CEP!");
+      setInput("");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className='title'>Buscador de CEP</h1>
+
+      <div className='containerInput'>
+        <input
+          type='text'
+          placeholder="Digite seu cep..."
+          value={input}
+          onChange={(evento) => setInput(evento.target.value)}
+        />
+
+        <button className="buttonSearch"
+          onClick={handleSearch}>
+          <FiSearch size={25} color="#FFF" />
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && (
+        <main className='main'>
+          <h2>CEP: {cep.cep}</h2>
+
+          <span>{cep.logradouro}</span>
+          {Object.keys(cep.complemento).length > 0 && (
+            <span>Complemento: {cep.complemento}</span>
+          )}
+          {Object.keys(cep.bairro).length > 0 && (
+          <span>{cep.bairro}</span>
+          )}
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
+
+
     </div>
   );
 }
